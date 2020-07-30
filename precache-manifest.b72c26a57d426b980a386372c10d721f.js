70dbd7ba680bb808d6be7d72e92d6b45be5b0feb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-30 06:52:04 +0000

    Deploy nleborgne/trivia to github.com/nleborgne/trivia.git:gh-pages

---
+++ precache-manifest.b72c26a57d426b980a386372c10d721f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edb0756a73342bd39e15f26e2e78f12",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "5d99cd11797e0e093919",
+    "url": "/trivia/static/css/main.981a38ae.chunk.css"
+  },
+  {
+    "revision": "a81ed7cfe58724c7c88a",
+    "url": "/trivia/static/js/2.656e69ac.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/trivia/static/js/2.656e69ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d99cd11797e0e093919",
+    "url": "/trivia/static/js/main.fe4faaa7.chunk.js"
+  },
+  {
+    "revision": "ef294555bde0349958cc",
+    "url": "/trivia/static/js/runtime-main.36b073f9.js"
+  }
+]);
